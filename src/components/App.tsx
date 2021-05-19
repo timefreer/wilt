b0424d285858a8422f5b42957e49aa799bc31d12
Micah Johnson <MJJohnson261@gmail.com>
2021-05-19 15:34:11 -0400

    Updated App component to use function notation and react hooks for state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,87 +2,67 @@ import './App.scss';
 import QuestionList from './QuestionList';
 import QuestionSchema from '../models/Question.schema';
 import Settings from './Settings';
-import React from 'react';
+import { useState } from 'react';
 import AddQuestion from './AddQuestion';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import defaultQuestions from '../data/default-questions';
 
-interface AppState {
-  questions: QuestionSchema[];
-}
+function App() {
+  const [questions, setQuestions] = useState(defaultQuestions);
 
-class App extends React.Component<{}, AppState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      questions: defaultQuestions
-    };
-
-    this.addQuestion = this.addQuestion.bind(this);
-    this.openAnswerForQuestion = this.openAnswerForQuestion.bind(this);
-    this.answerQuestion = this.answerQuestion.bind(this);
+  function addQuestion(newQuestion: QuestionSchema): void {
+    setQuestions([...questions, newQuestion]);
   }
 
-  addQuestion(newQuestion: QuestionSchema): void {
-    this.setState((state, props) => ({
-      questions: [...state.questions, newQuestion]
+  function openAnswerForQuestion(questionToAnswer: QuestionSchema): void {
+    setQuestions(questions.map(question => {
+      question.isAnswering = question.text === questionToAnswer.text;
+      return question;
     }));
   }
 
-  openAnswerForQuestion(questionToAnswer: QuestionSchema): void {
-    this.setState((state, props) => {
-      state.questions.forEach(question => question.isAnswering = question.text === questionToAnswer.text);
-      return { questions: [...state.questions] };
-    });
+  function answerQuestion(updatedQuestion: QuestionSchema): void {
+    setQuestions(questions.map(question => {
+      question.isAnswering = question.text === updatedQuestion.text ? false : question.isAnswering;
+      question.answer = question.text === updatedQuestion.text ? updatedQuestion.answer : question.answer;
+      return question;
+    }));
   }
 
-  answerQuestion(updatedQuestion: QuestionSchema): void {
-    this.setState((state, props) => {
-      let questionIndexToUpdate = state.questions.findIndex(question => question.text === updatedQuestion.text);
-      if (questionIndexToUpdate >= 0) {
-        state.questions[questionIndexToUpdate] = updatedQuestion;
-        state.questions[questionIndexToUpdate].isAnswering = false;
-        return { questions: [...state.questions] };
-      }
-    });
-  }
+  return (
+    <BrowserRouter>
+      <main className="wilt">
+        <Switch>
+          <Route path="/settings">
+            <Settings onSave={() => console.log('saved')} />
+          </Route>
 
-  render() {
-    return (
-      <BrowserRouter>
-        <main className="wilt">
-          <Switch>
-            <Route path="/settings">
-              <Settings onSave={() => console.log('saved')} />
-            </Route>
+          <Route path="/add-question">
+            <AddQuestion onAdd={addQuestion} />
+          </Route>
 
-            <Route path="/add-question">
-              <AddQuestion onAdd={this.addQuestion} />
-            </Route>
+          <Route exact path="/">
+            <QuestionList
+              questions={questions}
+              onClick={openAnswerForQuestion}
+              onAnswer={answerQuestion} />
 
-            <Route exact path="/">
-              <QuestionList
-                questions={this.state.questions}
-                onClick={this.openAnswerForQuestion}
-                onAnswer={this.answerQuestion} />
+            <Link to="/settings">
+              <button className="wilt__settings">
+                Settings
+              </button>
+            </Link>
 
-              <Link to="/settings">
-                <button className="wilt__settings">
-                  Settings
-                </button>
-              </Link>
-
-              <Link to="/add-question">
-                <button className="wilt__add-question">
-                  Add Question
-                </button>
-              </Link>
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
-    );
-  }
+            <Link to="/add-question">
+              <button className="wilt__add-question">
+                Add Question
+              </button>
+            </Link>
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
