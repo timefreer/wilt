388b9fa251a73cffcdceb98904a7f2250baa9980
Micah Johnson <MJJohnson261@gmail.com>
2021-05-19 15:54:03 -0400

    Updated AddQuestion component to use function notation and react hooks for state

--- src/components/AddQuestion.tsx
+++ src/components/AddQuestion.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import QuestionSchema from '../models/Question.schema';
 import './AddQuestion.scss';
@@ -7,63 +7,44 @@ interface AddQuestionProps {
     onAdd: (newQuestion: QuestionSchema) => void;
 }
 
-interface AddQuestionState {
-    questionText: string;
-    questionSubmitted: boolean;
-}
+function AddQuestion(props: AddQuestionProps) {
+    const [questionText, setQuestionText] = useState('');
+    const [questionSubmitted, setQuestionSubmitted] = useState(false);
 
-class AddQuestion extends React.Component<AddQuestionProps, AddQuestionState> {
-    constructor(props: AddQuestionProps) {
-        super(props);
-        this.state = {
-            questionText: '',
-            questionSubmitted: false
-        };
+    const showRedirect = () => questionSubmitted ? <Redirect to="/" /> : '';
 
-        this.onQuestionSubmit = this.onQuestionSubmit.bind(this);
-        this.showRedirect = this.showRedirect.bind(this);
-    }
-
-    onQuestionSubmit(event: React.FormEvent): void {
+    function onQuestionSubmit(event: React.FormEvent): void {
         event.preventDefault();
-        if (this.state.questionText) {
-            this.props.onAdd({
-                text: this.state.questionText,
-            });
-            this.setState({ questionSubmitted: true });
+        if (questionText) {
+            props.onAdd({ text: questionText });
+            setQuestionSubmitted(true);
         }
     }
 
-    showRedirect() {
-        return this.state.questionSubmitted ? <Redirect to="/" /> : '';
-    }
+    return (
+        <article className="add-question">
+            <h1>
+                Add a question
+            </h1>
 
-    render() {
-        return (
-            <article className="add-question">
-                <h1>
-                    Add a question
-                </h1>
+            <form onSubmit={onQuestionSubmit}>
+                <label>
+                    Question
+                    <input className="add-question__question"
+                        type="text"
+                        value={questionText}
+                        onChange={(event) => setQuestionText(event.target.value)} />
+                </label>
 
-                <form onSubmit={this.onQuestionSubmit}>
-                    <label>
-                        Question
-                        <input className="add-question__question"
-                            type="text"
-                            value={this.state.questionText}
-                            onChange={(event) => this.setState({ questionText: event.target.value })} />
-                    </label>
+                <button className="add-question__add-btn"
+                    type="submit">
+                    Add
+                </button>
+            </form>
 
-                    <button className="add-question__add-btn"
-                        type="submit">
-                        Add
-                    </button>
-                </form>
-
-                { this.showRedirect() }
-            </article>
-        );
-    }
+            { showRedirect() }
+        </article>
+    );
 }
 
 export default AddQuestion;
