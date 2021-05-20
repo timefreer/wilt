38c82b87e1268432475d193eda6ae2669fdc4b35
Micah Johnson <MJJohnson261@gmail.com>
2021-05-20 09:03:55 -0400

    Updated default questions isoDate field to always be current, and updated the default questions

--- src/data/default-questions.ts
+++ src/data/default-questions.ts
@@ -2,13 +2,16 @@ import QuestionSchema from "../models/Question.schema";
 
 const defaultQuestions: QuestionSchema[] = [
     {
-        isoDate: '2021-05-05',
-        text: 'What is the funniest thing today?',
+        isoDate: new Date().toISOString().split('T')[0],
+        text: 'What has been the funniest thing?',
         // answer: 'Lorem ipsum udle seedle is foa tuta'
     }, {
-        isoDate: '2021-05-05',
-        text: 'What is the best GIF?',
+        isoDate: new Date().toISOString().split('T')[0],
+        text: 'What has been the most interesting thing?',
         // answer: 'https://media.giphy.com/media/13ByqbM0hgfN7y/giphy.gif'
+    }, {
+        isoDate: new Date().toISOString().split('T')[0],
+        text: 'What has been the most important news?'
     }
 ];
 
