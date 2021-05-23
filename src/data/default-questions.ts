e992ebff5468e4ef7a415a4576b582aaa9f03873
Micah Johnson <MJJohnson261@gmail.com>
2021-05-23 10:01:26 -0400

    Refactored default-questions

--- src/data/default-questions.ts
+++ src/data/default-questions.ts
@@ -1,17 +1,19 @@
 import QuestionSchema from "../models/Question.schema";
 
+const todaysDate = new Date().toISOString().split('T')[0];
+
 const defaultQuestions: QuestionSchema[] = [
     {
-        isoDate: new Date().toISOString().split('T')[0],
+        isoDate: todaysDate,
         text: 'What has been the funniest thing?',
-        // answer: 'Lorem ipsum udle seedle is foa tuta'
+        // answer: 'Lorem ipsum udle seedle is foa tuta',
     }, {
-        isoDate: new Date().toISOString().split('T')[0],
+        isoDate: todaysDate,
         text: 'What has been the most interesting thing?',
-        // answer: 'https://media.giphy.com/media/13ByqbM0hgfN7y/giphy.gif'
+        // imageSrc: 'https://media.giphy.com/media/13ByqbM0hgfN7y/giphy.gif',
     }, {
-        isoDate: new Date().toISOString().split('T')[0],
-        text: 'What has been the most important news?'
+        isoDate: todaysDate,
+        text: 'What has been the most important news?',
     }
 ];
 
